261d6cf74bd05c5dc095e7b4e452c6c09b5b74f8
gessilt <henriquefreitas2710@gmail.com>
2022-03-01 21:48:10 -0300

    feat: Error management

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
-import express from "express";
+import express, {Request, Response, NextFunction} from "express";
 import "reflect-metadata";
 import "./database";
 import { router } from "./routes";
+import "express-async-errors";
 const app = express();
 
 /**
@@ -18,11 +19,20 @@ const app = express();
 app.get("/test", (request, response) => {
     //Request => Entrando
     //Response => Saindo
-    return response.send("OLá, Henrique");
+    return response.send("Olá, Henrique");
 });
 
 app.use(express.json());
 
 app.use(router);
+
+app.use((err:Error, request: Request, response: Response, next: NextFunction) => 
+{
+    if(err instanceof Error) {
+        return response.status(400).json({error: err.message});
+    } else {
+        return response.status(500).json({status: "error", message: "Internal Server Error"});
+    }
+})
 
 app.listen(3000, ()=> console.log('Servidor funcionando.'));
