5ce17e116950e411999935b057759ad04c4fad4f
gessilt <henriquefreitas2710@gmail.com>
2022-03-03 23:38:53 -0300

    feat: new middleware

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction, response } from "express";
+import { verify } from "jsonwebtoken";
+
+interface IPayLoad {
+    sub: string;
+}
+
+export function ensureAuthenticated (request: Request, reponse: Response, next: NextFunction) 
+{
+    const authToken = request.headers.authorization;
+
+    if(!authToken) {
+        return response.status(401).end();
+    }
+
+    const [,token] = authToken.split(" ");
+
+    try {
+        const {sub} = verify(token,"4f93ac9d10cv751b8c9c646bc9dbccb9") as IPayLoad;      
+        request.user_id = sub;
+        return next();
+    } catch (err) {
+        return response.status(401).end();
+    }
+
+
+
+
+}
