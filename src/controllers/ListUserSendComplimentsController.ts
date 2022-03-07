7145020ab49fce8e38748c69d7adc0cafd9ff897
gessilt <henriquefreitas2710@gmail.com>
2022-03-07 16:43:16 -0300

    refactor

--- src/controllers/ListUserSendComplimentsController.ts
+++ src/controllers/ListUserSendComplimentsController.ts
@@ -5,7 +5,8 @@ class ListUserSendComplimentsController {
     async handle(request: Request, response: Response) 
     {
         const {user_id} = request;
-        const listUserSendComplimentsService = new ListUserSendComplimentsService;
+        
+        const listUserSendComplimentsService = new ListUserSendComplimentsService();
 
         const compliments = await listUserSendComplimentsService.execute(user_id);
 
