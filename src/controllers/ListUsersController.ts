d031b1bc4deda3c4457fefaea06d7f6350ee7606
gessilt <henriquefreitas2710@gmail.com>
2022-03-07 16:42:30 -0300

    fix: request and response. Execute method.

--- src/controllers/ListUsersController.ts
+++ src/controllers/ListUsersController.ts
@@ -1,12 +1,13 @@
-import { Response } from "express";
+import { Response, Request } from "express";
 import { ListUsersService } from "../services/ListUsersService"
 
 
 class ListUsersController {
-    async handle(response: Response) {
-        const listUsersService = new ListUsersService;
+    async handle(request: Request, response: Response) {
+        
+        const listUsersService = new ListUsersService();
 
-        const users = listUsersService.execute();
+        const users = await listUsersService.execute();
 
         return response.json(users);
     }
